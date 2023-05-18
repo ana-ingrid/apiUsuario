4e683266323eb3786be284020903c7ca85a34b2b
AnaIngrid <ingridsantoscosta2003@gmail.com>
2023-05-18 17:39:16 -0300

    renomeando as classes de exceção

---
+++ cadastro/src/main/java/br/com/cadastro/exception/RecursoExistenteException.java
@@ -0,0 +1,12 @@
+package br.com.cadastro.exception;
+
+public class RecursoExistenteException extends Exception {
+
+
+	private static final long serialVersionUID = 1L;
+
+	public RecursoExistenteException(String string) {
+		super(string);
+	}
+	
+}
---
+++ cadastro/src/main/java/br/com/cadastro/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,13 @@
+package br.com.cadastro.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+
+
+	private static final long serialVersionUID = 1L;
+
+	public RecursoNaoEncontradoException(String string) {
+		super(string);
+	}
+	
+	
+}
